4142930c538f4a5bb39e4434319ab345cf61ddbf
Svetlana <svetarepalenko@yandex.ru>
2022-09-08 16:48:52 +0300

    done

---
+++ 1.1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет,
+//  является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+Console.WriteLine("Enter a five-digit number ");
+int x=Convert.ToInt32(Console.ReadLine());
+int x1=x;
+while (x1<10)  x1=x1/10;
+int x2=x%10;
+int x3=x/1000;
+x3=x3%10;
+int x4=x%100;
+x4/=10;
+if (x1==x2^x3==x4) Console.WriteLine("Palindrome true");
+else Console.WriteLine("Palindrome false");
---
+++ 1.2/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек 
+// и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+double getNum()
+{
+Console.Write("Enter a coordinate A x1: ");
+double x1=Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter a coordinate A y1: ");
+double y1=Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter a coordinate A z1: ");
+double z1=Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter a coordinate B x2: ");
+double x2=Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter a coordinate B y2: ");
+double y2=Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter a coordinate B z2: ");
+double z2=Convert.ToDouble(Console.ReadLine());
+double res=Math.Cbrt((x2-x1)*(x2-x1)*(x2-x1)+(y2-y1)*(y2-y1)*(y2-y1)+(z2-z1)*(z2-z1)*(z2-z1));
+return res;
+}
+
+try 
+{
+Console.WriteLine(getNum());
+}
+catch 
+{
+    Console.WriteLine("invalid input");
+}
